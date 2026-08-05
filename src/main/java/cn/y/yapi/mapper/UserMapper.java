4cbb88d716de7552368d394e58d57d6f79bc8cb2
yyyFG <2523766521@qq.com>
2026-08-05 16:41:16 +0800

    first commit

---
+++ src/main/java/cn/y/yapi/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package cn.y.yapi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import cn.y.yapi.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+

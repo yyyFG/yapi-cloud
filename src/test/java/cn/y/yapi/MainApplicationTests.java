080a2bbbf20b741ee2c2687491ec8f4c8a25d509
yyyFG <2523766521@qq.com>
2026-08-30 23:18:04 +0800

    实现测试用例，用来测试第三方SDK通过网关调用接口

--- src/test/java/cn/y/yapi/MainApplicationTests.java
+++ src/test/java/cn/y/yapi/MainApplicationTests.java
@@ -1,5 +1,8 @@
 package cn.y.yapi;
 
+
+import cn.y.yapiclientsdk.client.YApiClient;
+import cn.y.yapiclientsdk.model.InterfaceInfo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,6 +14,14 @@ class MainApplicationTests {
 
     @Test
     void contextLoads() {
+        YApiClient yApiClient = new YApiClient("a6b072c7a80671c2c1e0a9fae4da16e8", "152120b4bcab38863c3dfec04af96e29");
+        InterfaceInfo info = new InterfaceInfo();
+        info.setUrl("/api/u2092123055068327938/api/name/get");  // 对外 path
+        info.setMethod("GET");
+        info.setRequestParams("name=test");                      // 业务参数
+
+        String result = yApiClient.invokeInterface(info);
+        System.out.println(result);
     }
 
 }
